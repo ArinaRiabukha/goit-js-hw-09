623cc44287462e6a00b3927f62818b377bfe7892
Arina Riabukha <arrin.uq@gmail.com>
2024-11-19 14:28:49 +0200

    js files

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,46 @@
-console.log("Form");
+let formData= { 
+    email: "", 
+    message: "" 
+}
+
+const storageKey= "feedback-form-state";
+
+const form= document.querySelector('.feedback-form');
+const emailInput= form.elements.email;
+const messageInput= form.elements.message;
+
+function saveToLocalStorage() {
+    localStorage.setItem(storageKey, JSON.stringify(formData));
+  }
+function processInput(event) {
+    formData[event.target.name] = event.target.value;
+    saveToLocalStorage();
+  }
+  
+
+  function loadFromLocalStorage() {
+    const savedData= localStorage.getItem(storageKey);
+    if (savedData) {
+      formData= JSON.parse(savedData);
+      emailInput.value= formData.email;
+      messageInput.value= formData.message;
+    }
+  }
+  
+  function processSubmit(event) {
+    event.preventDefault();
+  
+    if (!formData.email.trim() || !formData.message.trim()) {
+      alert("Fill please all fields");
+      return;
+    }
+  
+    console.log(formData);
+
+    form.reset();
+    formData = { email: "", message: "" };
+    localStorage.removeItem(storageKey);
+  }
+
+form.addEventListener("input", processInput);
+form.addEventListener("submit", processSubmit);
